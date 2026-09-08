f165af389f6d61e986653f540f1a295de910361e
tanmayakaushal-hub <tanmayakaushal@gmail.com>
2026-09-08 21:49:24 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int commas = 0;
-        for(int i = 1000; i <= n; i++){
-            commas++;
-        }
-        return commas;
+        if(n < 1000) return 0;
+        return n - 999;
+        
     }
 };
